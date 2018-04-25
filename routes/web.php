9b38c55b60933da7236b6706e6793a7ef009484f
Landi <falandi@spartan.northampton.edu>
2018-04-25 17:41:13 -0400

    $ git add .

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//comment
+//comment is more comment
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
 
 Route::get('/about', function () {
     return view('about');
